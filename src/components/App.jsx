027abe019a02c62eb1445e7d04400f2d1e85c931
Krasnozhon Roman <105301264+KraRo16@users.noreply.github.com>
2022-11-20 17:26:24 +0100

    1/2/3 задачи

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Profile } from './Profile/Profile';
+import user from '../Data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import stat from '../Data/data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../Data/friends.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={stat} />
+      {/* <Statistics stats={stat} /> */}
+      <FriendList friends={friends} />;
+    </>
   );
 };
